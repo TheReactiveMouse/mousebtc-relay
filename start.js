70e0c15aab2e081257d5dd328c3ca2cb5f8bd552
TheReactiveMouse <40823060+TheReactiveMouse@users.noreply.github.com>
2020-06-24 21:41:26 +0600

    Update start.js

--- start.js
+++ start.js
@@ -1,14 +1,3 @@
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 80));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
 	var net = require('net');
 
 	var server = net.createServer(function(socket) {
@@ -17,4 +6,3 @@ app.get('/', function(request, response) {
 	});
 
 	server.listen(process.env.PORT || 703, '0.0.0.0');
-});
